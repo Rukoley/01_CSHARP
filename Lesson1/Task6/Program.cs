5817a64a7e338d9bc8f8415580052961e197ec8a
Stanislav Rukoleev <stasila713@mail.ru>
2024-03-01 14:26:33 +0500

    add lesson:task6

---
+++ Lesson1/Task6/Program.cs
@@ -0,0 +1,17 @@
+﻿static void Main(string[] args)
+ {
+    Console.Write("Введите целое число: ");
+     int a = int.Parse(Console.ReadLine());
+            
+     if (a % 2 == 0)
+     {
+     Console.WriteLine($"Число {a} является четным");
+     }
+ else
+    {
+     Console.WriteLine($"Число {a} является нечетным");
+    }
+}
+
+
+    
